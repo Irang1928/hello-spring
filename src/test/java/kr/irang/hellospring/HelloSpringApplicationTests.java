c624cfd5ec57591a49fee8e85667b8552421853f
Irang <yupk1928@naver.com>
2022-07-10 22:08:35 +0900

    first commit

---
+++ src/test/java/kr/irang/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.irang.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
