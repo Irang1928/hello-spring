d34c7aeb4c7400bd8ed50c8fad1c61d98ced58f2
Irang <yupk1928@naver.com>
2022-07-12 16:20:49 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/kr/irang/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/kr/irang/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package kr.irang.hellospring.repository;
 
 import kr.irang.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
--- src/main/java/kr/irang/hellospring/service/MemberService.java
+++ src/main/java/kr/irang/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package kr.irang.hellospring.service;
 import kr.irang.hellospring.domain.Member;
 import kr.irang.hellospring.repository.MemberRepository;
 import kr.irang.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
