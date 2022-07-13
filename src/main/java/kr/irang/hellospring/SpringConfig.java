213eec8839d9929deda4ce4d8b54742bf32e83a0
Irang <yupk1928@naver.com>
2022-07-13 21:14:53 +0900

    순수 JDBC

--- src/main/java/kr/irang/hellospring/SpringConfig.java
+++ src/main/java/kr/irang/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package kr.irang.hellospring;
 
+import kr.irang.hellospring.repository.JdbcMemberRepository;
 import kr.irang.hellospring.repository.MemberRepository;
 import kr.irang.hellospring.repository.MemoryMemberRepository;
 import kr.irang.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
