d0d2ebd819affdeab2b6b34980ff5f64e3e8b573
Irang <yupk1928@naver.com>
2022-07-14 16:50:01 +0900

    JPA

--- src/main/java/kr/irang/hellospring/SpringConfig.java
+++ src/main/java/kr/irang/hellospring/SpringConfig.java
@@ -2,22 +2,24 @@ package kr.irang.hellospring;
 
 import kr.irang.hellospring.repository.JdbcMemberRepository;
 import kr.irang.hellospring.repository.JdbcTemplatesMemberRepository;
+import kr.irang.hellospring.repository.JpaMemberRepository;
 import kr.irang.hellospring.repository.MemberRepository;
 import kr.irang.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -29,6 +31,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplatesMemberRepository(dataSource);
+        // return new JdbcTemplatesMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- src/main/java/kr/irang/hellospring/service/MemberService.java
+++ src/main/java/kr/irang/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import kr.irang.hellospring.repository.MemberRepository;
 import kr.irang.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
